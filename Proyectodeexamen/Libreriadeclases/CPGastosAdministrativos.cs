0d378d3219d973882c4a35f44baad1c14c44c3ed
mastergamer1309 <josiaszeledon20@gmail.com>
2024-06-29 23:11:56 -0600

    crear las clases de la biblioteca

--- Proyectodeexamen/Libreriadeclases/CPGastosAdministrativos.cs
+++ Proyectodeexamen/Libreriadeclases/CPGastosAdministrativos.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace BibliotecaDeClasess
 {
-    internal class CPPresupuestoMaestro
+    internal class CPGastosAdministrativos
     {
     }
 }
--- Proyectodeexamen/Libreriadeclases/CPMod.cs
+++ Proyectodeexamen/Libreriadeclases/CPMod.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace BibliotecaDeClasess
 {
-    internal class CPGastos
+    internal class CPMod
     {
     }
 }
