0551771fc2f0d4e03fb1c0df2b229c409114c09e
Mastergamer1309 <123322718+mastergamer1309@users.noreply.github.com>
2024-06-29 21:48:23 -0600

    .
    .

---
+++ Proyectodeexamen/Proyecto_Examen/FrmPantallaPrincipal.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Proyecto_Examen
+{
+    /// <summary>
+    /// Interaction logic for FrmPantallaPrincipal.xaml
+    /// </summary>
+    public partial class FrmPantallaPrincipal : Window
+    {
+        public FrmPantallaPrincipal()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnPresupuestoMaestro_Click(object sender, RoutedEventArgs e)
+        {
+            FrmPantallaPrincipal frmPantallaPrincipal = new FrmPantallaPrincipal();
+            FrmPresupuestoMaestro frmPresupuestoMaestro = new FrmPresupuestoMaestro();
+
+            frmPresupuestoMaestro.Show();
+            
+            this.Close();
+
+        }
+    }
+}
